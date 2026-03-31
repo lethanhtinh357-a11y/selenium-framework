fa02769b844dd254dbb7a792e4fa4870297e5073
Le Tinh <1150080160@sv.hcmunre.edu.vn>
2026-03-31 15:21:42 +0700

    fix

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,28 +12,19 @@ public class LoginTest extends BaseTest {
     @Test(retryAnalyzer = RetryAnalyzer.class)
     public void testLoginSuccess() {
 
-        // mở trang demo
-        driver.get("https://the-internet.herokuapp.com/login");
+        // đúng site dùng credential từ Secrets
+        driver.get("https://www.saucedemo.com/");
 
-        // lấy username/password từ ENV (GitHub Secrets)
         String username = ConfigReader.getUsername();
         String password = ConfigReader.getPassword();
 
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password); // sẽ bị *** trên CI
-
-        // nhập username
-        driver.findElement(By.id("username")).sendKeys(username);
-
-        // nhập password
+        driver.findElement(By.id("user-name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
-
-        // click login
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        driver.findElement(By.id("login-button")).click();
 
         // verify login thành công
-        String message = driver.findElement(By.id("flash")).getText();
+        boolean isDisplayed = driver.findElement(By.className("inventory_list")).isDisplayed();
 
-        Assert.assertTrue(message.contains("You logged into a secure area!"));
+        Assert.assertTrue(isDisplayed);
     }
 }
