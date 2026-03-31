9b1d9639da56f91bf2e27a81072f361cc733ac8f
Le Tinh <1150080160@sv.hcmunre.edu.vn>
2026-03-31 14:44:07 +0700

    complete bai 2

--- src/test/java/common/DriverFactory.java
+++ src/test/java/common/DriverFactory.java
@@ -1,8 +1,14 @@
 package common;
 
 import org.openqa.selenium.WebDriver;
+
+// Chrome
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+// Firefox
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -10,10 +16,8 @@ public class DriverFactory {
 
     public static WebDriver createDriver(String browser) {
 
-        // đọc biến môi trường CI
         boolean isCI = System.getenv("CI") != null;
 
-        // đọc browser từ command line (CI truyền vào)
         if (browser == null || browser.isEmpty()) {
             browser = System.getProperty("browser", "chrome");
         }
@@ -21,22 +25,35 @@ public class DriverFactory {
         System.out.println("Running browser: " + browser);
         System.out.println("Running in CI: " + isCI);
 
-        // setup Chrome
-        WebDriverManager.chromedriver().setup();
+        switch (browser.toLowerCase()) {
 
-        ChromeOptions options = new ChromeOptions();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions ffOptions = new FirefoxOptions();
 
-        if (isCI) {
-            // chạy trên GitHub Actions
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--window-size=1920,1080");
-        } else {
-            // chạy local
-            options.addArguments("--start-maximized");
+                if (isCI) {
+                    ffOptions.addArguments("--headless");
+                    ffOptions.addArguments("--width=1920");
+                    ffOptions.addArguments("--height=1080");
+                }
+
+                return new FirefoxDriver(ffOptions);
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+
+                if (isCI) {
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--window-size=1920,1080");
+                } else {
+                    options.addArguments("--start-maximized");
+                }
+
+                return new ChromeDriver(options);
         }
-
-        return new ChromeDriver(options);
     }
 }
