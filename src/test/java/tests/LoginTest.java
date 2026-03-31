8ef3b0b80d1f6e1ec8fb77dcb430bd3b07437552
Le Tinh <1150080160@sv.hcmunre.edu.vn>
2026-03-31 15:35:31 +0700

    fix

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,38 +4,34 @@ import common.BaseTest;
 import common.RetryAnalyzer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
 
     @Test(retryAnalyzer = RetryAnalyzer.class)
     public void testLoginSuccess() {
 
-        // mở trang saucedemo (đúng với secret)
         driver.get("https://www.saucedemo.com/");
 
-        // lấy từ GitHub Secrets
         String username = ConfigReader.getUsername();
         String password = ConfigReader.getPassword();
 
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password); // sẽ hiển thị *** trên CI
-
-        // nhập username
         driver.findElement(By.id("user-name")).sendKeys(username);
-
-        // nhập password
         driver.findElement(By.id("password")).sendKeys(password);
-
-        // click login
         driver.findElement(By.id("login-button")).click();
 
-        // verify login thành công
-        boolean isInventoryDisplayed =
-                driver.findElement(By.className("inventory_list")).isDisplayed();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        Assert.assertTrue(isInventoryDisplayed, "Login failed!");
+        boolean isDisplayed = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.className("inventory_list"))
+        ).isDisplayed();
+
+        Assert.assertTrue(isDisplayed, "Login failed!");
     }
 }
