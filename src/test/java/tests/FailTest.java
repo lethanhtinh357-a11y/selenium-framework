73516ebf4bb5119b33a0ce77bad9c15d0a41e8ec
Le Tinh <1150080160@sv.hcmunre.edu.vn>
2026-03-31 15:32:23 +0700

    final 1 2 3

--- src/test/java/tests/FailTest.java
+++ src/test/java/tests/FailTest.java
@@ -1,18 +1,12 @@
 package tests;
 
-import common.BaseTest;
-import common.RetryAnalyzer;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class FailTest extends BaseTest {
+public class FailTest {
 
-    @Test(retryAnalyzer = RetryAnalyzer.class)
-    public void testFail() {
-
-        driver.get("https://example.com");
-
-        // cố tình fail để test retry + screenshot
-        Assert.assertTrue(false);
+    @Test
+    public void testPass() {
+        Assert.assertTrue(true);
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,6 +3,7 @@ package tests;
 import common.BaseTest;
 import common.RetryAnalyzer;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
@@ -12,19 +13,29 @@ public class LoginTest extends BaseTest {
     @Test(retryAnalyzer = RetryAnalyzer.class)
     public void testLoginSuccess() {
 
-        // đúng site dùng credential từ Secrets
+        // mở trang saucedemo (đúng với secret)
         driver.get("https://www.saucedemo.com/");
 
+        // lấy từ GitHub Secrets
         String username = ConfigReader.getUsername();
         String password = ConfigReader.getPassword();
 
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password); // sẽ hiển thị *** trên CI
+
+        // nhập username
         driver.findElement(By.id("user-name")).sendKeys(username);
+
+        // nhập password
         driver.findElement(By.id("password")).sendKeys(password);
+
+        // click login
         driver.findElement(By.id("login-button")).click();
 
         // verify login thành công
-        boolean isDisplayed = driver.findElement(By.className("inventory_list")).isDisplayed();
+        boolean isInventoryDisplayed =
+                driver.findElement(By.className("inventory_list")).isDisplayed();
 
-        Assert.assertTrue(isDisplayed);
+        Assert.assertTrue(isInventoryDisplayed, "Login failed!");
     }
 }
