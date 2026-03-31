75aebe7d3a686136777b7016646d4d9285fa7e0a
Le Tinh <1150080160@sv.hcmunre.edu.vn>
2026-03-31 15:38:34 +0700

    fix

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,7 +3,6 @@ package tests;
 import common.BaseTest;
 import common.RetryAnalyzer;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -22,16 +21,31 @@ public class LoginTest extends BaseTest {
         String username = ConfigReader.getUsername();
         String password = ConfigReader.getPassword();
 
+        System.out.println("USER = " + username);
+        System.out.println("PASS = " + password);
+
         driver.findElement(By.id("user-name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login-button")).click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        boolean isDisplayed = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.className("inventory_list"))
-        ).isDisplayed();
+        try {
+            // thử tìm inventory (login thành công)
+            boolean isDisplayed = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.className("inventory_list"))
+            ).isDisplayed();
 
-        Assert.assertTrue(isDisplayed, "Login failed!");
+            Assert.assertTrue(isDisplayed);
+
+        } catch (Exception e) {
+
+            // 👉 nếu fail → in ra lỗi login
+            String error = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+
+            System.out.println("LOGIN ERROR: " + error);
+
+            Assert.fail("Login failed: " + error);
+        }
     }
 }
